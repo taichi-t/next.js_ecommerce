f089c27b41ce757aa5049a325daf1c3eefd10e75
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-09-15 18:20:17 -0700

    delte: useless code

--- utils/formatImagePublicIds.js
+++ utils/formatImagePublicIds.js
@@ -1,5 +1,4 @@
 export default function formatImagePublicIds(urls) {
-  // http://res.cloudinary.com/dhwqisywt/image/upload/v1600165511/pzk2j31hrh2ejkyq86u5.jpg
   const publicIds = urls.map((url) => {
     let key;
     key = url.split('/')[7];
