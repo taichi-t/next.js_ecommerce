761b23acc7ccfe8d45b0db7a25408271e81515e7
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-09-17 04:05:06 -0700

    modify:baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://garagesale.now.sh'
+    ? 'https://private-garage-sale.vercel.app'
     : 'http://localhost:3000';
 
 export default baseUrl;
