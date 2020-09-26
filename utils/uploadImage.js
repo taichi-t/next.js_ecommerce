b09716369f86008494d60de31315c174a22a871b
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-09-26 15:34:33 -0700

    refactor:uploadimage

--- utils/uploadImage.js
+++ utils/uploadImage.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 
 export const uploadImage = async (medias) => {
-  const data = new FormData();
   try {
     // let promises = [];
     let requests = [];
     console.log(process.env.CLOUDINARY_URL);
 
     for (let i = 0; i < medias.length; i++) {
+      let data = new FormData();
       data.append('file', medias[i]);
       data.append('upload_preset', 'reactreserve');
-      data.append('cloud_name', 'reedbargercodes');
+      data.append('cloud_name', `${process.env.CLOUDINARY_CLOUD_NAME}`);
       requests.push(axios.post(process.env.CLOUDINARY_URL, data));
     }
     console.log(requests);
     const responses = await axios.all(requests);
-    const mediaUrls = responses.map((response) => response.data.url);
+    const mediaUrls = await responses.map((response) => response.data.url);
     console.log({ mediaUrls });
     return mediaUrls;
   } catch (error) {
