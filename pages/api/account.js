a7584835b948d3476e4c3d157d372e89da9e492a
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-09-30 19:48:28 -0700

    delete:console.log

--- pages/api/account.js
+++ pages/api/account.js
@@ -39,7 +39,6 @@ handler.get(async (req, res) => {
 });
 
 handler.put(async (req, res) => {
-  console.log(req);
   const { _id, role } = req.body;
   await User.findOneAndUpdate({ _id }, { role });
   res.status(203).send('User updated');
