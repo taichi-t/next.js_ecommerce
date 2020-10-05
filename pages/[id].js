aa5cac4abf064217aff76644e02eedf32720308a
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-10-04 19:38:59 -0700

    fix:bug

--- pages/[id].js
+++ pages/[id].js
@@ -1,13 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useReducer } from 'react';
 import ProductSummary from '../components/Product/ProductSummary';
 import ProductAttributes from '../components/Product/ProductAttributes';
 import getProductPaths from '../utils/getProductPaths';
 import getProduct from '../utils/getProduct';
 import { UserContext } from '../utils/UserProvider';
+import { useRouter } from 'next/router';
 
 function Product({ product }) {
+  const router = useRouter();
   const { user } = useContext(UserContext);
-  return (
+  return router.isFallback ? (
+    <div>Loading</div>
+  ) : (
     <>
       <ProductSummary user={user} product={product && product[0]} />
       <ProductAttributes user={user} product={product && product[0]} />
@@ -25,7 +29,6 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params: { id } }) {
   const product = await getProduct(id);
-  console.log(product);
   return {
     props: {
       product,
--- utils/getProduct.js
+++ utils/getProduct.js
@@ -31,7 +31,6 @@ export default async function getProduct(id) {
           },
         },
       },
-
       { $unwind: '$user' },
     ]);
     return JSON.parse(JSON.stringify(product));
