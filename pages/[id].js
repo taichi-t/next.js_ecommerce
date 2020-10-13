1e127ca19a0ccecd37a6e2c1e8c549fd3a13d924
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-10-13 15:39:57 -0700

    test: await res.json()

--- pages/[id].js
+++ pages/[id].js
@@ -47,7 +47,7 @@ export async function getStaticProps({ params: { id } }) {
   // const { data } = await axios.get(url, { params: { id } });
   // console.log('getStaticProps', data);
   const res = await fetch(`${url}?id=${id}`);
-  const json = res.json();
+  const json = await res.json();
 
   return {
     props: {
